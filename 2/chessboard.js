b6498705a27cced47c51ceda421108b74d1827ae
Jose Pagan <jose@joses-mbp.home>
2018-03-06 01:10:44 +0000

    size feature added

--- 2/chessboard.js
+++ 2/chessboard.js
@@ -1,10 +1,11 @@
 //Write a program that creates a string that represents an 8x8 grid, using new line character to separate lines.
 //At each position of the grid there is either a space or a "#" character. The characters should form a chess board.
 
+var size = 8;
 var boardString = "";
-for (var line = 0; line < 8; line++) {  //main loop that is executed for each line
+for (var line = 0; line < size; line++) {  //main loop that is executed for each line
 var boardLine = "";
-  while (boardLine.length < 8) {  //loop keeps adding spaces and hush until necesary lenth
+  while (boardLine.length < size) {  //loop keeps adding spaces and hush until necesary lenth
     if (line % 2 == 0) {
       boardLine += "# ";
     } else {
