8675f333f0304ae253bd2f236212021146a10893
Jose Pagan <jose@Joses-MacBook-Pro.local>
2018-03-12 12:53:27 +0000

    minimun problem finished

---
+++ 3/minimun.js
@@ -0,0 +1,12 @@
+//Write a function min that takes two arguments and returns their minimum.
+
+const min = function(a, b) {
+  if (a <= b) {
+    return a;
+  } else {
+    return b
+  }
+}
+
+console.log(min(2,4),
+min(4,2))
