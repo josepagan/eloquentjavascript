322cc4f1fe75320eaa3d6228d3be425d01db8e62
Jose Pagan <jose@joses-mbp.home>
2018-05-24 09:55:17 +0100

    added flattening

---
+++ 5/flattening.js
@@ -0,0 +1,5 @@
+//Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+let newArray = arrays.reduce((a,b) => a.concat(b));
+console.log(newArray);
